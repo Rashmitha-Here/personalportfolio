357ba2624deba9b04ef8b1cfd11bc5345f80d64d
Rashmitha M <109420219+Rashmitha-Here@users.noreply.github.com>
2023-11-18 18:46:41 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text", {
-    strings: ["B.Tech CSE Final Year Student 🎯", "well in Front-End Web Developement", "well in Coding", "well in Problem Solver" ],
+    strings: ["B.Tech CSE Final Year Student 🎯", "Front-End Web Developement", "well in Coding", "well in Problem Solver" ],
     typeSpeed: 90,
     backSpeed: 70,
     backDelay: 50,
