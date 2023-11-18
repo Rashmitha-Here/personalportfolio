7c81c3ad63e24fe1bf527999e7a24d46394aab01
Rashmitha M <109420219+Rashmitha-Here@users.noreply.github.com>
2023-11-18 19:50:52 +0530

    added main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text", {
-    strings: ["And I'm a B.Tech CSE Final Year Student.", "I'm Interested in Front-End WebDevelopement.", "I'm Interested in Coding", "I'm Interested in Problem Solving..." ],
+    strings: ["And I'm a B.Tech CSE Final Year Student", "I'm Interested in Front-End WebDevelopement", "I'm Interested in Coding", "I'm Interested in Problem Solving..." ],
     typeSpeed: 90,
     backSpeed: 70,
     backDelay: 50,
