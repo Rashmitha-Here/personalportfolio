74c9c71dd13a009527a7ab40cb960ceff92c7dbe
Rashmitha M <109420219+Rashmitha-Here@users.noreply.github.com>
2023-11-18 13:05:26 +0530

    added main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text", {
+    strings: ["B.Tech CSE Final Year Student 🎯", "well in Front-End Web Developement", "well in Coding", "well in Problem Solver" ],
+    typeSpeed: 90,
+    backSpeed: 70,
+    backDelay: 50,
+    loop: true
+});
+
